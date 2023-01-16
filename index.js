dd53dadc5e47f1f2d269819320503a8c0d175863
James541 <96335020+James541@users.noreply.github.com>
2023-01-15 23:56:08 -0800

    added window to js to fuction with netlify

--- index.js
+++ index.js
@@ -8,28 +8,34 @@ function hPlusOne() {
     homeCount += 1
     homeScoreEl.innerText = homeCount
 }
+window.hPlusOne = hPlusOne
 
 function hPlusTwo() {
     homeCount += 2
     homeScoreEl.innerText = homeCount
 }
+window.hPlusTwo = hPlusTwo
 
 function hPlusThree() {
     homeCount += 3
     homeScoreEl.innerText = homeCount
 }
+window.hPlusThree = homeOne
 
 function gPlusOne() {
     guestCount += 1
     guestScoreEl.innerText = guestCount
 }
+window.gPlusOne = gPlusOne
 
 function gPlusTwo() {
     guestCount += 2
     guestScoreEl.innerText = guestCount
 }
+window.gPlusTwo = gPlusTwo
 
 function gPlusThree() {
     guestCount += 3
     guestScoreEl.innerText = guestCount
 }
+window.gPlusThree = gPlusThree
